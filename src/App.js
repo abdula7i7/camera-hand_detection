2a99211c503481cb501b893fe266a20d17626a12
abdulaziz <8enior@gmail.com>
2023-11-03 18:20:34 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        let videoElement = document.getElementById('video');
+        videoElement.srcObject = stream;
+      })
+      .catch(error => {
+        console.error('Hatolik yuz berdi:', error);
+      });
+  }, []);
+
+  const rasmgaOl = () => {
+
+    let videoElement = document.getElementById('video');
+    let canvas = document.createElement('canvas');
+
+    // canvas.width = videoElement.videoWidth;
+    // canvas.height = videoElement.videoHeight;
+
+    let context = canvas.getContext('2d')
+    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+    let images = document.querySelector('.images')
+    let li = document.createElement('li')
+    let imageElement = document.createElement('img')
+    images.append(li)
+    li.append(imageElement)
+    imageElement.src = canvas.toDataURL();
+
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className='camera'>
+        <video id='video' autoPlay></video>
+        <div className='camera-btn'>
+          <button id="capture-btn" onClick={rasmgaOl}></button>
+        </div>
+      </div>
+      <ul className='images'>
+
+      </ul>
     </div>
   );
 }
